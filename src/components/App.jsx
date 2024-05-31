3247ac55f559bce4812d966dd7a50f8bafa693c2
Ashish Kunal <ashishkunal678@gmail.com>
2024-06-01 00:29:19 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+
+function App() {
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: "",
+  });
+  const [content, setContent] = useState("");
+
+  function handleChange(event) {
+    const { value, name } = event.target;
+
+    setContact((prevValue) => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: prevValue.lName,
+          email: prevValue.email,
+        };
+      } else if (name === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: value,
+          email: prevValue.email,
+        };
+      } else if (name === "email") {
+        return {
+          fName: prevValue.fName,
+          lName: prevValue.lName,
+          email: value,
+        };
+      }
+    });
+  }
+
+  function handleClick(event) {
+    setContent(contact);
+    event.preventDefault();
+  }
+
+  return (
+    <div className="container">
+      <h1>
+        Hello {content.fName} {content.lName}
+      </h1>
+      <p>{content.email}</p>
+      <form onSubmit={handleClick}>
+        <input
+          onChange={handleChange}
+          name="fName"
+          placeholder="First Name"
+          value={contact.fName}
+        />
+        <input
+          onChange={handleChange}
+          name="lName"
+          placeholder="Last Name"
+          value={contact.lName}
+        />
+        <input
+          onChange={handleChange}
+          name="email"
+          placeholder="Email"
+          value={contact.email}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
